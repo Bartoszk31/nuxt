6ca4663124aa27c93155d8e0544475c43ad90ab7
bkwiecien <bartosz.kwiecien@wakacje.pl>
2023-02-07 11:23:45 +0100

    init init

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    Some default layout shared across all pages
+    <div>
+      <NuxtLink to="/about">About</NuxtLink>
+      <NuxtLink to="/">Home</NuxtLink>
+    </div>
+    <slot />
+  </div>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+<script setup>
+const { pending, data } = useLazyFetch("/api/test");
+</script>
+
+<template>
+  <div v-if="pending">Loading</div>
+  <div v-else>asasas{{ data }}</div>
+</template>
